cb863ed89e61a84a243cdc7fd142f6b413c5c960
Elijah Rivers <leeb467ma@gmail.com>
2022-03-21 21:58:41 -0400

    created working game reset

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('#grid');
     let boxes = Array.from(document.querySelectorAll('.box'));
-    const Score = document.querySelector('#score');
-    const StartBtn = document.querySelector('#start-button');
+    const Health = document.querySelector('#score');
+    const StartBtn = document.querySelector('#start_button');
     const width = 20;
 
     const RedRoki = [1,width,width+1,width+2,width*2+1];
@@ -11,13 +11,30 @@ document.addEventListener('DOMContentLoaded', () => {
     let redPosition = Math.floor(Math.random()*18);
     let bluePosition = Math.floor(Math.random()*18);
 
-    draw =()=> {
-        RedRoki.forEach(index => {
-            boxes[redPosition + index].classList.add('redRoki')
-        })
-        BlueRoki.forEach(index => {
-            boxes[bluePosition + index].classList.add('blueRoki')
-        })
-    }
-    draw();
+    StartBtn.onclick = function() {init()};
+
+    init =()=> {
+        undrawRoki =()=> {
+            RedRoki.forEach(index => {
+                boxes[redPosition + index].classList.remove('redRoki')
+            })
+            BlueRoki.forEach(index => {
+                boxes[bluePosition + index].classList.remove('blueRoki')
+            })
+        }
+
+        drawRoki =()=> {
+            RedRoki.forEach(index => {
+                boxes[redPosition + index].classList.add('redRoki')
+            })
+            BlueRoki.forEach(index => {
+                boxes[bluePosition + index].classList.add('blueRoki')
+            })
+        }
+        undrawRoki();
+        redPosition = Math.floor(Math.random()*18);
+        bluePosition = Math.floor(Math.random()*18);
+        drawRoki();
+        StartBtn.innerHTML = "Reset Game";
+    } 
 })
